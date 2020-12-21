1cc52d2dbdd28ac36bbd1e6219517115d88e5422
Your Name <romanchhetri02@gmail.com>
2020-12-21 18:31:07 +0545

    local db added

--- resturant/admin.py
+++ resturant/admin.py
@@ -1,23 +1,21 @@
 from django.contrib import admin
-from .models import Resturant,Address,Menu,ItemType,Images
+from .models import *
 
-
+class ReviewsInline(admin.StackedInline):
+    model = Review
+    extra=0
 
 class MenuInline(admin.StackedInline):
     model=Menu
     extra = 0
 
+
 # class AddressInline(admin.StackedInline):
 #     model = Address
 
 class ResturantAdmin(admin.ModelAdmin):
-    list_display=('name','address')
-    fieldsets = [
-        (None,{'fields':['name']}),
-        ('Bio',{'fields':['description']}),
-        ('Address',{'fields':['address']}),
-    ]
-    inlines = [MenuInline]
+    list_display=('name','address','stars_average')
+    inlines = [MenuInline,ReviewsInline]
 
 # admin.site.register([Resturant,Address,Menu,ItemType,Images])
 admin.site.register(Resturant,ResturantAdmin)
--- resturant/models.py
+++ resturant/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.db.models import Avg
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Address(models.Model):
     district=models.CharField(max_length=50)
@@ -34,15 +36,25 @@ class Resturant(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=500,blank=True)
     cover_photo=models.ImageField(upload_to=get_cover_image_filename)
-    delivery_availabel = models.BooleanField(default=False)
     address = models.OneToOneField(Address, on_delete=models.CASCADE,related_name='address')
+    delivery_available = models.BooleanField(default=False)
     open_time = models.TimeField(blank=True,null=True)
     close_time = models.TimeField(blank=True,null=True)
-    contact = models.OneToOneField(Contact,null=True,on_delete=models.CASCADE)
+    contact = models.OneToOneField(Contact,null=True,on_delete=models.CASCADE,blank=True)
 
     def __str__(self):
         return self.name
 
+    def stars_average(self):
+        self.review_set.aggregate(Avg('stars')).values()[0]
+    stars_average.short_description='Ratings'
+
+class Review(models.Model):
+    user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    resturant = models.ForeignKey(Resturant,on_delete=models.CASCADE,)
+    review = models.TextField(max_length=500,blank=True,null=True)
+    stars = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)],null=True,blank=True)
+    reviewed_on = models.DateField(auto_now_add=True,null=True,blank=True)
 
 def get_menu_image_filename(instance, filename):
     title = instance.resturant.name
